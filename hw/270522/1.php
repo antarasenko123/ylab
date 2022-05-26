9b8b3c10938d2a84a4a094f9558a28e4b81b4297
Andrey Tarasenko <antarasenko123@gmail.com>
2022-05-26 21:46:24 +0300

    доработана первая задача

--- hw/270522/1.php
+++ hw/270522/1.php
@@ -1,29 +1,30 @@
 <?php
+/*
+Задача №1. У вас есть массив. Необходимо отсортировать его по PRICE
+*/
 
-# 1) У вас есть массив. Необходимо отсортировать его по PRICE
-for($i=1; $i <= 5; $i++) { // 5 складов
-    for($i1=1; $i1 < 3; $i1++) { // по 2 товара в каждой категории - Еда и Напитки
+for($i=1; $i <= 5; $i++) { // создаем массив, заполняем товарами
+    for($i1=1; $i1 <= 3; $i1++) {
         $array['SKLAD'.$i]['EDA']['TOVAR'.$tovar_counter++] = ['NAME' => 'EDA', 'PRICE' => mt_rand(1,1000)];
         $array['SKLAD'.$i]['NAPITKI']['TOVAR'.$tovar_counter++] = ['NAME' => 'NAPITOK', 'PRICE' => mt_rand(1,1000)];
     }
 }
 
-foreach($array as $sklad) {
-    echo "<pre>";
-        print_r ($sklad);
-    echo "</pre>";
-    foreach($sklad as $id => $group) {
-        echo $id."<br>";
-        print_r($group);
-        //echo $group;
+/*
+Перебираем созданный массив и создаем новый массив с ключом = ID товара и
+значением = Цена товара
+*/
+
+foreach($array as $sklad_id => $items_group) {
+    foreach($items_group as $id => $items) {
+        foreach($items as $id => $item) {
+            $tovar_list[$id] = $item['PRICE'];
+        }
     }
 }
 
-echo "<pre>";
-    print_r($array);
-echo "</pre>";
-
-
-
+// сортируем массив по убыванию цены, т.к. порядок сортировки в задаче не указан
+arsort($tovar_list);
+echo "<pre>"; print_r($tovar_list); echo "</pre>";
 
 ?>
