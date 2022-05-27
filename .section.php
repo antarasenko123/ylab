77331e2e52aaee543744e8de66e2b462b91f37b7
Andrey Tarasenko <antarasenko123@gmail.com>
2022-05-27 13:33:04 +0300

    first commit

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная страница";
+$arDirProperties = array(
+   "description" => "Мы предлагаем широкий ассортимент по адекватным ценам.",
+   "keywords" => "купить",
+   "robots" => "index, follow"
+);
+?>
